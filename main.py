0644c7b79cc41a9a5a5dd5a3b5fc5e56af5ff121
Michaela Alexandridi <michaela.alex03@gmail.com>
2026-02-15 16:18:29 +0200

    Printing results and plots for Dynamic Analysis

--- main.py
+++ main.py
@@ -54,12 +54,22 @@ K_r, M_r, f_r, free = apply_bc(K, M, f, fixed_dofs)
 
 # Solve System
 u = static_solver(K_r, f_r)
+
 wL_numerical = u[-2]
-wL_analytical, _ = analytical_solution(P, L, E, I)
+thetaL_numerical = u[-1]
+wL_analytical, thetaL_analytical = analytical_solution(P, L, E, I)
+
+error_wL = abs(wL_numerical-wL_analytical)/abs(wL_analytical)
+error_thetaL = abs(thetaL_numerical-thetaL_analytical)/abs(thetaL_analytical)
 
 # Check results
 print(f"Static Tip Displacement FEM: {wL_numerical:.6e}")
-print(f"Static Tip Displacement FEM: {wL_analytical:.6e}")
+print(f"Static Tip Displacement ΑΝΑ: {wL_analytical:.6e}")
+print(f"Absolute error for {n_elem} elements: {error_wL:.6e}")
+
+print(f"Static Tip Rotation FEM: {thetaL_numerical:.6e}")
+print(f"Static Tip Rotation ΑΝΑ: {thetaL_analytical:.6e}")
+print(f"Absolute error for {n_elem} elements: {error_thetaL:.6e}")
 
 
 
@@ -91,10 +101,12 @@ plt.show()
 # Omega
 omega_numerical = frequency_estimation(t, uh[:,-2], min_height=0.0, min_distance=0.08)
 omega_analytical = (1.875**2) * np.sqrt(E*I/(rho*A*L**4))
+est_error = abs(omega_numerical-omega_analytical)/omega_analytical
+per_error = 100*est_error
 
 print(f"ω1 FEM: {omega_numerical:.3f}")
 print(f"ω1 ANA: {omega_analytical:.3f}")
-
+print(f"Percentage Error: {per_error:.3f}%")
 
 
 """
@@ -105,7 +117,8 @@ print(f"ω1 ANA: {omega_analytical:.3f}")
 
 # Input
 P0 = 1e3
-omega = 0.95*omega_numerical                                      # Given frequency
+omega = 0.95*omega_numerical  # Given frequency
+time = 1.5
 
 """
 # A way for harmonic force
@@ -113,7 +126,6 @@ omega = 0.95*omega_numerical                                      # Given freque
 force = lambda t: (np.eye(len(u0))[-2] * P0 * np.sin(omega*t))    # Time-varying load
 
 """
-
 
 # B way for calculating harmonic force
     
@@ -129,16 +141,59 @@ new_u0 = np.zeros_like(u0)
 new_v0 = np.zeros_like(new_u0)
 
 # Newmark Integration
-new_t, new_uh, new_vh, new_ah = newmark(M_r, K_r, force, new_u0, new_v0, dt, t_end)
+new_t, new_uh, new_vh, new_ah = newmark(M_r, K_r, force, new_u0, new_v0, dt, time)
 
 # Create GIF file for u
 animate_beam(new_uh,
              free_dofs=free,
              n_elem=n_elem,
              L=L,
-             time=t,
+             time=new_t,
              scale=55,
              filename="semfebeam.gif",
              fps0=30,
              max_frames=150,
              show=True)
+
+
+# Plot for u(L)
+plt.figure(figsize=(9,4))
+plt.plot(new_t,new_uh[:,-2])
+plt.xlabel("t")
+plt.ylabel("w(L)")
+plt.title("Displacement of free end")
+plt.show()
+
+# Plot for u(L/2)
+mid_node = int(n_elem / 2) * 2 - 2
+
+plt.figure(figsize=(9,4))
+plt.plot(new_t,new_uh[:,mid_node])
+plt.xlabel("t")
+plt.ylabel("w(L/2)")
+plt.title("Displacement in the middle")
+plt.show()
+
+# Plot phase diagram
+plt.figure(figsize=(9,4))
+plt.plot(new_uh[:, -2], new_vh[:, -2])
+plt.xlabel("Displacement w(L)")
+plt.ylabel("Velocity v(L)")
+plt.title("Phase Diagram at Free End")
+plt.show()
+
+# Plot energies
+K = 0.5 * np.sum((new_vh @ M_r) * new_vh, axis=1)
+Udynamic = 0.5 * np.sum((new_uh @ K_r) * new_uh, axis=1)
+E_tot = K + Udynamic
+
+plt.figure(figsize=(9, 4))
+plt.plot(new_t, K, label='Kinetic Energy (K)', color='red')
+plt.plot(new_t, Udynamic, label='Elastic Energy (U)', color='blue')
+plt.plot(new_t, E_tot, label='Total Energy (E_tot)', color='black', linestyle='--')
+plt.xlabel("t (s)")
+plt.ylabel("Energy (Joules)")
+plt.title("Energy Variations over Time")
+plt.legend()
+plt.grid(True)
+plt.show()
